adb7e5aea2ab738a4e2319b03e8d4bb7a776d56d
Jonathan <jowjapao@gmail.com>
2017-06-08 18:44:04 -0300

    Criação table oficinas

---
+++ database/migrations/2017_06_08_213309_create_oficinas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOficinasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oficinas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nome', 100);
+            $table->longText('descricao');
+            $table->integer(vagasdisponiveis);
+            $table->integer(vagasreservas);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oficinas');
+    }
+}
