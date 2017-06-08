c70a407c8c24f62f443badd4f76bee93ebfa60f7
Jonathan <jowjapao@gmail.com>
2017-06-08 19:20:51 -0300

    Criado a table denúncias

---
+++ database/migrations/2017_06_08_221740_create_denuncias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDenunciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('denuncias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nome', 100);
+            $table->char('email', 100);
+            $table->char('telefone', 15);
+            $table->char('assunto', 100);
+            $table->longText('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('denuncias');
+    }
+}
