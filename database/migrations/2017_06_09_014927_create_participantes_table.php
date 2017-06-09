8338d64fb902f56ecfb08534cc7fa845bd96fc5b
Jonathan <jowjapao@gmail.com>
2017-06-08 22:51:20 -0300

    Criando outro table do CRUD participantes de oficinas, porque deu ruim no CPF, errei no instanciamento da variável no table

---
+++ database/migrations/2017_06_09_014927_create_participantes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nome', 100);
+            $table->char('cpf', 14);
+            $table->char('oficina', 50);
+            $table->char('endereco', 100);
+            $table->char('telefone', 15);
+            $table->char('bairro', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participantes');
+    }
+}
