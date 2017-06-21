631d53c4bcab21dbb5d7fef8ee5714c322b4f25b
Jonathan <jowjapao@gmail.com>
2017-06-21 12:28:29 -0300

    Table agendamentos reajustada

---
+++ database/migrations/2017_06_21_150753_create_agendamentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 100);
+            $table->string('email');
+            $table->dateTime('horario');
+            $table->text('assunsto');
+            $table->longText('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+}
